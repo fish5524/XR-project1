2a5b9e029a697ba599664797059def556072af12
deeelol <tony200404242@gmail.com>
2026-04-08 21:15:21 +0800

    StartScene

---
+++ Assets/Scripts/StartGameButtonHandler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StartGameButtonHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Start Game button clicked!");
+    }
+}
